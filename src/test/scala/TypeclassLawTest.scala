3ed2a63dac93c56e29c5e3724520252e718a8f51
xuwei-k <6b656e6a69@gmail.com>
2014-02-02 04:16:56 +0900

    more random test for Cobind[IArray]

--- src/test/scala/TypeclassLawTest.scala
+++ src/test/scala/TypeclassLawTest.scala
@@ -3,6 +3,7 @@ package iarray
 import scalaz._
 import std.anyVal._, std.string._
 import scalaz.scalacheck.ScalazProperties._
+import org.scalacheck.{Gen, Arbitrary}
 
 object IArrayTraverseTest extends TestCommon with ScalacheckOps {
 
@@ -19,12 +20,34 @@ object IArrayTraverseTest extends TestCommon with ScalacheckOps {
 
 object TypeclassLawTest extends TestCommon {
 
+  implicit val iarrayArb0: Arbitrary[IArray[Int] => Int] = {
+    def arb(f: (IArray[Int], Int) => Int): Gen[IArray[Int] => Int] =
+      implicitly[Arbitrary[Int]].arbitrary.map(a => xs => f(xs, a))
+
+    Arbitrary(Gen.oneOf(
+      arb((_ , x) => x),
+      arb((xs, x) => xs.headOption getOrElse x),
+      arb((xs, x) => xs.lastOption getOrElse x),
+      arb((xs, _) => xs.sum),
+      arb((xs, x) => xs.max.getOrElse(x)),
+      arb((xs, x) => xs.min.getOrElse(x)),
+      arb((xs, x) => if(xs.nonEmpty) xs(xs.length / 2) else x),
+      arb((xs, x) => xs.headOption.map(_ + x).getOrElse(- x)),
+      arb((xs, x) => xs.lastOption.map(_ + x).getOrElse(- x)),
+      arb((xs, _) => xs.size)
+    ))
+  }
+
+  property("cobind associative") =
+    cobind.cobindAssociative[IArray, Int, Int, Int, Int](
+      implicitly, implicitly, implicitly, iarrayArb0, iarrayArb0, iarrayArb0
+    )
+
   checkAll(monadPlus.strongLaws[IArray])
   checkAll(isEmpty.laws[IArray])
   checkAll(foldable.laws[IArray])
   checkAll(zip.laws[IArray])
   checkAll(align.laws[IArray])
-  checkAll(cobind.laws[IArray])
   checkAll(monoid.laws[IArray[String]])
 
 }
